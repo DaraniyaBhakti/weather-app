a75f4a6f9ab3a5c1854d7b5adf4f737ccf0104f8
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-06-23 18:17:40 +0530

    first-commit

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+export const colors = {
+    PRIMARY_COLOR: '#ff304f',
+    SECONDARY_COLOR: '#002651',
+    BORDER_COLOR: '#dbdbdb',
+}
+
+
+
+// http://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=fae7190d7e6433ec3a452
+// 85ffcf55c86"		
